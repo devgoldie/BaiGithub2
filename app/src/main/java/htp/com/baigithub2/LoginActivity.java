997837e51d7298b930bf4e79d9711c1f18f933a6
devgoldie <htp061195@gmail.com>
2017-05-15 01:02:19 +0700

    commit changed

---
+++ app/src/main/java/htp/com/baigithub2/LoginActivity.java
@@ -0,0 +1,10 @@
+package htp.com.baigithub2;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by HP-SIN on 5/15/2017.
+ */
+
+public class LoginActivity extends AppCompatActivity {
+}
---
+++ app/src/main/java/htp/com/baigithub2/Student.java
@@ -0,0 +1,8 @@
+package htp.com.baigithub2;
+
+/**
+ * Created by HP-SIN on 5/15/2017.
+ */
+
+public class Student {
+}
